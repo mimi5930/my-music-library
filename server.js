f54c1510a686ea373efb399576bf04ec169215aa
mimi5930 <mikej.miller440@gmail.com>
2022-06-20 20:38:05 -0500

    initial server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
