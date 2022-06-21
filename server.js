6ce35c001164f9791875ea73f58c317677c80eb2
mimi5930 <mikej.miller440@gmail.com>
2022-06-20 20:55:22 -0500

    gql setup

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schemas';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
