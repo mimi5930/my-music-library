f8929db3029802418ad70512e260ec391affd69a
mimi5930 <mikej.miller440@gmail.com>
2022-06-27 13:15:18 -0500

    composers search gql

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,3 +1,15 @@
-const resolvers = {};
+const fetch = require('node-fetch');
 
-module.exports = { resolvers };
+const resolvers = {
+  Query: {
+    composer: async (parent, { name }) => {
+      const composerData = await fetch(
+        `https://api.openopus.org/composer/list/search/${name}.json`
+      );
+      const data = await composerData.json();
+      return data;
+    }
+  }
+};
+
+module.exports = resolvers;
--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,5 +1,40 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Status {
+    version: String
+    success: String
+    error: String
+    source: String
+    rows: Int
+    processingtime: Float
+    api: String
+  }
 
-module.exports = { typeDefs };
+  type Request {
+    type: String
+    item: String
+  }
+
+  type Composers {
+    id: ID
+    name: String
+    complete_name: String
+    birth: String
+    death: String
+    epoch: String
+    portrait: String
+  }
+
+  type Composer {
+    status: Status
+    request: Request
+    composers: [Composers!]
+  }
+
+  type Query {
+    composer(name: String!): Composer
+  }
+`;
+
+module.exports = typeDefs;
