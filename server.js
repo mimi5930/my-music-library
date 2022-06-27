53e79f23d1bb89e28718683b55461d7cb7612421
mimi5930 <mikej.miller440@gmail.com>
2022-06-27 14:25:12 -0500

    switched completely to graphql

--- server.js
+++ server.js
@@ -25,46 +25,6 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/composer/:comp', async (req, res) => {
-  const { comp } = req.params;
-  const response = await fetch(
-    `https://api.openopus.org/composer/list/search/${comp}.json`
-  );
-  const data = await response.json();
-  if (data.status.success === 'false')
-    res.status(404).json({ message: data.status.error });
-  if (data.status.rows > 1)
-    res
-      .status(404)
-      .json({ message: 'Multiple composers found, refine search' });
-  res.json(data);
-});
-
-app.get('/composer/:comp/piece/:piece', async (req, res) => {
-  const { comp, piece } = req.params;
-  // find composer
-  const response = await fetch(
-    `https://api.openopus.org/composer/list/search/${comp}.json`
-  );
-  const data = await response.json();
-  if (data.status.success === 'false')
-    res.status(404).json({ message: data.status.error });
-  if (data.status.rows > 1)
-    res
-      .status(404)
-      .json({ message: 'Multiple composers found, refine search' });
-  // save composer's id
-  const compId = data.composers[0].id;
-
-  // find piece with comp Id and title
-  const pieceResponse = await fetch(
-    `https://api.openopus.org/work/list/composer/${compId}/genre/all/search/${piece}.json`
-  );
-  const pieceData = await pieceResponse.json();
-
-  res.json(pieceData);
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
